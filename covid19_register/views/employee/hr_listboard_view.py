cfa07d17ebf6ddbb778bd5c03af71051b7448202
ame diphoko <diphokoame@gmail.com>
2020-10-15 09:53:05 +0200

    update hr queryset filter

--- covid19_register/views/employee/hr_listboard_view.py
+++ covid19_register/views/employee/hr_listboard_view.py
@@ -8,11 +8,11 @@ class HrListBoardView(BaseListBoardView):
     search_form_url = 'hr_listboard_url'
 
     def get_context_data(self, **kwargs):
-        context = super().get_context_data(site_name='hr', **kwargs)
+        context = super().get_context_data(site_name='hr_finance', **kwargs)
         return context
 
     def get_queryset_filter_options(self, request, *args, **kwargs):
         options = super().get_queryset_filter_options(request,
-                                                      site_name='hr',
+                                                      site_name='hr_finance',
                                                       *args, **kwargs)
         return options
